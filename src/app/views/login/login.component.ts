414819a4da6f243bfb9dd86b79fa7cdbbf118247
giwi97 <giwanthawijekoon@gmail.com>
2020-12-15 01:10:59 +0530

    Changes done in login page

--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
-  templateUrl: 'login.component.html'
+  templateUrl: 'login.component.html',
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent { }
