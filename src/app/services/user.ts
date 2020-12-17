7e555247f7b0247aaf41810c076c0f47146f5486
giwi97 <giwanthawijekoon@gmail.com>
2020-12-17 23:40:02 +0530

    User class created

---
+++ src/app/services/user.ts
@@ -0,0 +1,23 @@
+export class User {
+  id: number;
+  nic: string;
+  name: string;
+  company: string;
+  mobile: number;
+  city: string;
+  password: string;
+
+
+  constructor() { }
+
+//   constructor (id: number, nic: string, name: string, company: string, mobile: number, city: string, password: string) {
+
+//     this.id = id;
+//     this.nic = nic;
+//     this.name = name;
+//     this.company = company;
+//     this.mobile = mobile;
+//     this.city = city;
+//     this.password = password;
+//   }
+}
