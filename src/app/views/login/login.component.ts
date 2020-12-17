14c8d98a3df91396e2bf0312086f43fc6d448932
giwi97 <giwanthawijekoon@gmail.com>
2020-12-17 23:40:27 +0530

    Created ngFrom and function

--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -1,8 +1,20 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { RegistrationService } from '../../services/registration.service';
+import { User } from '../../services/user';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: 'login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent { }
+export class LoginComponent {
+
+  user = new User();
+  constructor(private _service: RegistrationService) { }
+
+  loginUser() {
+    this._service.loginUserFromRemote();
+  }
+
+ }
