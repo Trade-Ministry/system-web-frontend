30a6c4769904c890efc94854486869e092ad3ec2
giwi97 <giwanthawijekoon@gmail.com>
2020-12-18 02:11:35 +0530

    Injected rgistration service and route

--- src/app/views/register/register.component.ts
+++ src/app/views/register/register.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { RegistrationService } from '../../services/registration.service';
+import { User } from '../../services/user';
+
 @Component({
   selector: 'app-dashboard',
   templateUrl: 'register.component.html',
@@ -7,6 +12,23 @@ import { Component } from '@angular/core';
 })
 export class RegisterComponent {
 
-  constructor() { }
+  user = new User();
+  message = '';
+
+  constructor(private _service: RegistrationService, private _router: Router) { }
+
+  registerUser() {
+    this._service.registerUserFromRemote(this.user).subscribe(
+      data => {
+        console.log('Response received');
+        this.message = 'Registration Succussful';
+        this._router.navigate(['/login']);
+      },
+      error => {
+        console.log('Exception occured');
+        this.message = 'Error';
+      }
+    );
+  }
 
 }
