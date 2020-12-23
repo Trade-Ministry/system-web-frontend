ad1caa5e9c09a26ee863c133737b1de493a866c8
giwi97 <giwanthawijekoon@gmail.com>
2020-12-23 10:16:52 +0530

    Getting all products method

--- src/app/services/prices.service.ts
+++ src/app/services/prices.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Products } from './products';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PricesService {
 
-  constructor() { }
+  constructor( private _http: HttpClient ) { }
+
+  public addProductFromRemote(products: Products): Observable<any> {
+    return this._http.post<any>('http://localhost:8080/addprice', products);
+  }
+
+  public getProductsFromRemote(): Observable<any>{
+    return this._http.get<any>('http://localhost:8080/addprice');
+  }
+
 }
