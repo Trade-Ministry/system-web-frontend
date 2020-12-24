7147f36e1946db4d3700367be8734b84c60efbb7
giwi97 <giwanthawijekoon@gmail.com>
2020-12-24 23:58:35 +0530

    Profile component

--- src/app/views/profile/profile.component.ts
+++ src/app/views/profile/profile.component.ts
@@ -6,29 +6,8 @@ import { getStyle, rgbToHex } from '@coreui/coreui/dist/js/coreui-utilities';
   templateUrl: 'profile.component.html'
 })
 export class ProfileComponent implements OnInit {
-  constructor(@Inject(DOCUMENT) private _document: any) {}
-
-  public themeColors(): void {
-    Array.from(this._document.querySelectorAll('.theme-color')).forEach((el: HTMLElement) => {
-      const background = getStyle('background-color', el);
-      const table = this._document.createElement('table');
-      table.innerHTML = `
-        <table class="w-100">
-          <tr>
-            <td class="text-muted">HEX:</td>
-            <td class="font-weight-bold">${rgbToHex(background)}</td>
-          </tr>
-          <tr>
-            <td class="text-muted">RGB:</td>
-            <td class="font-weight-bold">${background}</td>
-          </tr>
-        </table>
-      `;
-      el.parentNode.appendChild(table);
-    });
-  }
-
   ngOnInit(): void {
-    this.themeColors();
+    throw new Error('Method not implemented.');
   }
+
 }
