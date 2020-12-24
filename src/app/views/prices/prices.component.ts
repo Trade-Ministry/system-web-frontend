f427ed620062285bb8e7c6da845eee515bd6ff72
giwi97 <giwanthawijekoon@gmail.com>
2020-12-25 00:00:06 +0530

    manually adding prices

--- src/app/views/prices/prices.component.ts
+++ src/app/views/prices/prices.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { PricesService } from '../../services/prices.service';
+import { Products } from '../../services/products';
+
 @Component({
   selector: 'app-prices',
   templateUrl: './prices.component.html',
@@ -7,7 +12,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PricesComponent implements OnInit {
 
-  constructor() { }
+  products = new Products();
+  message = '';
+
+  constructor(private _service: PricesService, private _router: Router) { }
+
+
+  addPrice() {
+    this._service.addProductFromRemote(this.products).subscribe(
+      data => {
+        console.log('Respose received');
+        this.message = 'Price added successfully';
+        this._router.navigate(['/dashboard']);
+      },
+      error => {
+        console.log('Exception occured');
+        this.message = 'Error';
+      }
+    );
+  }
 
   ngOnInit(): void {
   }
--- src/app/views/prices/prices.module.ts
+++ src/app/views/prices/prices.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 
 import { PricesComponent } from './prices.component';
 import { PricesRoutingModule } from './prices-routing.module';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   imports: [
-    PricesRoutingModule
+    PricesRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   declarations: [ PricesComponent ]
 })
