d5e56b2f289f46c7305d10db44040629678c84e0
giwi97 <giwanthawijekoon@gmail.com>
2020-12-30 03:01:28 +0530

    HttpClient Module

--- src/app/views/upload/upload.module.ts
+++ src/app/views/upload/upload.module.ts
@@ -4,12 +4,14 @@ import { NgxDropzoneModule } from 'ngx-dropzone';
 import { UploadComponent } from './upload.component';
 import { UploadRoutingModule } from './upload-routing.module';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   imports: [
     UploadRoutingModule,
     NgxDropzoneModule,
-    CommonModule
+    CommonModule,
+    HttpClientModule
   ],
   declarations: [ UploadComponent ]
 })
