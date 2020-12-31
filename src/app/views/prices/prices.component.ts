e5d85084096eb113bf0b8c353e40dc9d12cef806
giwi97 <giwanthawijekoon@gmail.com>
2020-12-31 19:04:50 +0530

    replaced with ng-select

--- src/app/views/prices/prices.component.ts
+++ src/app/views/prices/prices.component.ts
@@ -1,3 +1,4 @@
+import { NgForOf } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 
 import { NgForm } from '@angular/forms';
@@ -14,6 +15,14 @@ export class PricesComponent implements OnInit {
 
   products = new Products();
   message = '';
+  categories = [
+    { id: 1, val: 'එළවළු', name: 'Vegetables(එළවළු)' },
+    { id: 2, val: 'පළතුරු', name: 'Fruits(පළතුරු)' },
+    { id: 3, val: 'ධාන්‍යය', name: 'Grains(ධාන්‍යය)' },
+    { id: 4, val: 'පලාවර්ග', name: 'Leafy Greens(පලාවර්ග)' },
+    { id: 5, val: 'අලවර්ග', name: 'Yams(අලවර්ග)' },
+    { id: 6, val: 'අනෙකුත්', name: 'Other(අනෙකුත්)..' },
+  ];
 
   constructor(private _service: PricesService, private _router: Router) { }
 
--- src/app/views/prices/prices.module.ts
+++ src/app/views/prices/prices.module.ts
@@ -4,12 +4,18 @@ import { PricesComponent } from './prices.component';
 import { PricesRoutingModule } from './prices-routing.module';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { NgSelectModule } from '@ng-select/ng-select';
+import { CommonModule } from '@angular/common';
+import { NgOptionHighlightModule } from '@ng-select/ng-option-highlight';
 
 @NgModule({
   imports: [
     PricesRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    NgSelectModule,
+    CommonModule,
+    NgOptionHighlightModule
   ],
   declarations: [ PricesComponent ]
 })
