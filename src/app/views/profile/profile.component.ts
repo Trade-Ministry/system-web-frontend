5b21bd45a714da75ffae38d73cc6bd9f41b22008
giwi97 <giwanthawijekoon@gmail.com>
2021-01-01 01:57:30 +0530

    functions for updating and getting current users

--- src/app/views/profile/profile.component.ts
+++ src/app/views/profile/profile.component.ts
@@ -1,13 +1,46 @@
 import { Component, Inject, OnInit } from '@angular/core';
-import { DOCUMENT } from '@angular/common';
+import { DOCUMENT, NgForOf } from '@angular/common';
 import { getStyle, rgbToHex } from '@coreui/coreui/dist/js/coreui-utilities';
+import { User } from '../../services/user';
+import { RegistrationService } from '../../services/registration.service';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+
 
 @Component({
   templateUrl: 'profile.component.html'
 })
 export class ProfileComponent implements OnInit {
+
+  _users: User;
+  users = new User();
+  message = '';
+
+  constructor(private _service: RegistrationService, private _router: Router) {}
+
+
+  updateUser() {
+    this._service.updateUserFromRemote(this.users).subscribe(
+      data => {
+        console.log('Response Received');
+        this.message = 'Updated';
+        this._router.navigate(['profile/editprofile']);
+      },
+      error => {
+        console.log('Exception occured');
+        this.message = 'Error';
+      }
+    );
+  }
+
   ngOnInit(): void {
-    throw new Error('Method not implemented.');
+    this._service.getUserFromRemote().subscribe(
+      data => {
+        console.log('Response Recieved');
+        this._users = data;
+      },
+      error => console.log('Error Occured')
+    );
   }
 
 }
