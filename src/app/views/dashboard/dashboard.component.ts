2856be48d84f0b8d0aaeda41ae1019d626085d62
giwi97 <giwanthawijekoon@gmail.com>
2021-01-04 01:39:44 +0530

    fetching categories

--- src/app/views/dashboard/dashboard.component.ts
+++ src/app/views/dashboard/dashboard.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { getStyle, hexToRgba } from '@coreui/coreui/dist/js/coreui-utilities';
 import { CustomTooltips } from '@coreui/coreui-plugin-chartjs-custom-tooltips';
+import { Products } from '../../services/products';
+import { PricesService } from '../../services/prices.service';
 
 @Component({
   templateUrl: 'dashboard.component.html'
@@ -8,6 +10,9 @@ import { CustomTooltips } from '@coreui/coreui-plugin-chartjs-custom-tooltips';
 export class DashboardComponent implements OnInit {
 
   radioModel: string = 'Month';
+  _categorylist: Products[];
+
+  constructor(private _service: PricesService) {}
 
   // lineChart1
   public lineChart1Data: Array<any> = [
@@ -211,7 +216,7 @@ export class DashboardComponent implements OnInit {
 
   // mainChart
 
-  public mainChartElements = 27;
+  public mainChartElements = 7;
   public mainChartData1: Array<number> = [];
   public mainChartData2: Array<number> = [];
   public mainChartData3: Array<number> = [];
@@ -236,7 +241,7 @@ export class DashboardComponent implements OnInit {
     }
   ];
   /* tslint:disable:max-line-length */
-  public mainChartLabels: Array<any> = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday', 'Monday', 'Thursday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+  public mainChartLabels: Array<any> = ['28 Dec', '29 Dec', '30 Dec', '31 Dec', '01 Jan', '02 Jan', '03 Jan'];
   /* tslint:enable:max-line-length */
   public mainChartOptions: any = {
     tooltips: {
@@ -257,11 +262,6 @@ export class DashboardComponent implements OnInit {
       xAxes: [{
         gridLines: {
           drawOnChartArea: false,
-        },
-        ticks: {
-          callback: function(value: any) {
-            return value.charAt(0);
-          }
         }
       }],
       yAxes: [{
@@ -385,7 +385,32 @@ export class DashboardComponent implements OnInit {
     return Math.floor(Math.random() * (max - min + 1) + min);
   }
 
+  public getLastWeek(): Array<any> {
+
+    const ourDate = new Date();
+    const myweek: Array<any> = [];
+
+    for (let i = 0; i <= 6; i++) {
+      const full = new Date(ourDate.setDate(ourDate.getDate() - 1));
+      const day = full.getDay();
+      const month = full.getMonth();
+      const yr = full.getFullYear();
+      const compl = `${day}/${month}/${yr}`;
+      myweek.push(day);
+    }
+    return myweek;
+  }
+
   ngOnInit(): void {
+
+    this._service.getCategoriesFromRemote().subscribe(
+      data => {
+        console.log('Response Recieved: Category');
+        this._categorylist = data.map;
+      },
+      error => console.log('Error Occured: Category')
+    );
+
     // generate random values for mainChart
     for (let i = 0; i <= this.mainChartElements; i++) {
       this.mainChartData1.push(this.random(50, 150));
@@ -393,5 +418,6 @@ export class DashboardComponent implements OnInit {
       this.mainChartData3.push(this.random(65, 110));
       this.mainChartData4.push(this.random(30, 140));
     }
+    console.log('Week', this.getLastWeek());
   }
 }
