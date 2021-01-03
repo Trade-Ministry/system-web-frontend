ad23267961e51d419bc11f3572d150dc42fecd54
giwi97 <giwanthawijekoon@gmail.com>
2021-01-04 01:38:58 +0530

    Session storage

--- src/app/services/registration.service.ts
+++ src/app/services/registration.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { from, Observable } from 'rxjs';
 import { User } from './user';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +12,19 @@ export class RegistrationService {
   constructor( private _http: HttpClient ) { }
 
   public loginUserFromRemote(user: User): Observable<any> {
-    return this._http.post<any>('http://localhost:8080/login', user);
+    return this._http.post<any>('http://localhost:8080/login', user).pipe(
+      map(
+        userData => {
+          sessionStorage.setItem('nic', user.nic);
+          return userData;
+        }
+      )
+    );
+  }
+
+  isUserLoggedIn() {
+    const user = sessionStorage.getItem('nic');
+    return !(user == null);
   }
 
   public registerUserFromRemote(user: User): Observable<any> {
